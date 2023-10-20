4461a690f3f0622ec68d97cd5d937e062fe3cff1
Michle99 <sammiyonyx@gmail.com>
2023-10-20 10:29:36 -0400

    Created javascript folder, added javascript labs and assessments to folder.

---
+++ JavaScript Modules/javascript-lab-one/src/index.mjs
@@ -0,0 +1,7 @@
+
+document.getElementById("app").innerHTML = `
+<h1 style="text-align: center; font-weight: bold;"> CSX Precourse - Part 2 section Completed</h1>
+<div style="text-align: center; font-weight: bold;">
+    JavaScript Lab One
+</div>
+`;
